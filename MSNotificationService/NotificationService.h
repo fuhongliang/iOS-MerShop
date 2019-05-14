e9a22013ab34f90108e796a0ca956385662ca64f
chenli <1035924121@qq.com>
2019-05-14 16:33:48 +0800

    第三方跑马灯

---
+++ MSNotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  MSNotificationService
+//
+//  Created by mac on 2019/5/13.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
